47c0d4d0255f436504d870d57436f75f9ea542f8
Apaar gupta <apaar.gupta.13@gmail.com>
2015-07-01 18:46:01 +0530

    minor change in header file

--- userspace/Include/pru_bridge.h
+++ userspace/Include/pru_bridge.h
@@ -7,7 +7,7 @@
 #define PRU_READ 0
 #define PRU_WRITE 1
 
-extern int pru_bridge_init(int channel_sizes[NUM_CHANNELS]);
+int pru_bridge_init(int channel_sizes[NUM_CHANNELS]);
 void pru_channel_open(int channel_no,int type);
 void pru_channel_close(int channel_no);
 int pru_write(int channel_no,uint8_t* pru_data,uint8_t length);
